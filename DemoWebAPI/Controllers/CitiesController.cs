9eaa399223b16b938a1ffa682a8fbca4b01cd590
Marilyne Naili <marilyne.lydia@gmail.com>
2024-02-18 22:37:48 +0100

    initial commit

---
+++ DemoWebAPI/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebAPI.Controllers
+{
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<Object>
+                {
+                    new {id=1, name="New York City"},
+                    new {id=2, name="Antwerp"}
+                }
+                );
+        }
+    }
+}
